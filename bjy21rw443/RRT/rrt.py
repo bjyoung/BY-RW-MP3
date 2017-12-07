f0130921ea0cc43b10d8d320349ab03b1b33d6eb
bjyoung <bjyoung20591@gmail.com>
2017-12-07 16:35:17 -0500

    Optimized find_closest_edge()
    Only checked edges attached to closest point, instead of checking every
    [label1, label2] edge pairing

--- bjy21rw443/RRT/rrt.py
+++ bjy21rw443/RRT/rrt.py
@@ -207,13 +207,14 @@ def is_between_endpts(intersect_pt, edge):
     epsilon = 0.01 # Because equivalence may not work with decimals
     return True if math.fabs(dist_ab - (dist_ac + dist_cb)) <= 0.01 else False
 
-def find_closest_edge(new_point, points, adjListMap):
+def find_closest_edge(new_point, closest_point, points, adjListMap):
     """
     Find the closest edge to the given point based on the adjListMap
     If there are no edges on the map, return -1 as the point and 5000 as the min_dist
     If there is an edge, give the two endpoints that form the edge and the intersecting pt
 
     :param new_point: label of new point to add to map
+    :param closest_point: label of the closest point in the tree to the new point
     :param points: map point labels to their coordinates
     :param adjListMap: map from point labels to their neighbors
 
@@ -223,25 +224,25 @@ def find_closest_edge(new_point, points, adjListMap):
     min_dist = 5000 # represents infinity, since 10x10 grid
     closest_intersect_pt = (-1, -1)
 
-    if has_no_edges(adjListMap):
+    neighbor_labels = adjListMap[closest_point]
+    if not neighbor_labels: # Check if there are any edges from closest_point
         return closest_edge, min_dist, closest_intersect_pt
 
-    # For every edge (label-neighbor pair), find dist from new point to the edge
+    # For every edge (label-neighbor pair) from the closest point, find dist from new point to the edge
     # Update min edge if the distance found is smaller
-    for label in adjListMap:
-        for neighbor_label in adjListMap[label]:
-            edge = [label, neighbor_label]
-            dist, intersect_pt = get_distance_to_edge(new_point, edge, points)
+    for neighbor_label in neighbor_labels:
+        edge = [closest_point, neighbor_label]
+        dist, intersect_pt = get_distance_to_edge(new_point, edge, points)
 
-            # Check if intersect pt is between edge's endpoints, if not, then throw out edge
-            edge_coordinates = [points[label], points[neighbor_label]]
-            if not is_between_endpts(intersect_pt, edge_coordinates):
-                continue
+        # Check if intersect pt is between edge's endpoints, if not, then throw out edge
+        edge_coordinates = [points[closest_point], points[neighbor_label]]
+        if not is_between_endpts(intersect_pt, edge_coordinates):
+            continue
 
-            if dist < min_dist:
-                closest_edge = edge
-                min_dist = dist
-                closest_intersect_pt = intersect_pt
+        if dist < min_dist:
+            closest_edge = edge
+            min_dist = dist
+            closest_intersect_pt = intersect_pt
 
     return closest_edge, min_dist, closest_intersect_pt
 
@@ -265,7 +266,7 @@ def growSimpleRRT(points):
 
         # Find closest point and edge
         closest_point, closest_pt_dist = find_closest_point(point, newPoints, adjListMap)
-        closest_edge, closest_edge_dist, closest_intersect_pt = find_closest_edge(label, newPoints, adjListMap)
+        closest_edge, closest_edge_dist, closest_intersect_pt = find_closest_edge(label, closest_point, newPoints, adjListMap)
 
         # If point, then add as neighbor to the closest point
         if closest_pt_dist <= closest_edge_dist:
@@ -294,9 +295,9 @@ def growSimpleRRT(points):
             adjListMap[intersect_label] = [endpt_a, endpt_b, label]
 
         # # FOR TESTING, visualize tree after every step
-        # displayRRTandPath(newPoints, adjListMap, None)
+        displayRRTandPath(newPoints, adjListMap, None)
 
-    return newPoints, adjListMap
+    # return newPoints, adjListMap
 
 '''
 Perform basic search 
@@ -604,24 +605,24 @@ def RRT(robot, obstacles, startPoint, goalPoint):
     return points, tree, path
 
 if __name__ == "__main__":
-    # Retrive file name for input data
-    if(len(sys.argv) < 6):
-        print "Five arguments required: python spr.py [env-file] [x1] [y1] [x2] [y2]"
-        exit()
+    # # Retrive file name for input data
+    # if(len(sys.argv) < 6):
+    #     print "Five arguments required: python spr.py [env-file] [x1] [y1] [x2] [y2]"
+    #     exit()
+    #
+    # filename = sys.argv[1]
+    # x1 = float(sys.argv[2])
+    # y1 = float(sys.argv[3])
+    # x2 = float(sys.argv[4])
+    # y2 = float(sys.argv[5])
 
-    filename = sys.argv[1]
-    x1 = float(sys.argv[2])
-    y1 = float(sys.argv[3])
-    x2 = float(sys.argv[4])
-    y2 = float(sys.argv[5])
-
-    # # FOR TESTING REMOVE LATER--------------------------------------------------------------------------
-    # filename = "robot_env_01.txt"
-    # x1 = 1.0
-    # y1 = 2.0
-    # x2 = 8.5
-    # y2 = 7
-    # #----------------------------------------------------------------------------------------------------
+    # FOR TESTING REMOVE LATER--------------------------------------------------------------------------
+    filename = "robot_env_01.txt"
+    x1 = 1.0
+    y1 = 2.0
+    x2 = 8.5
+    y2 = 7
+    #----------------------------------------------------------------------------------------------------
 
     # Read data and parse polygons
     lines = [line.rstrip('\n') for line in open(filename)]
@@ -688,23 +689,23 @@ if __name__ == "__main__":
     points[20] = (9, 0.6)
 
     # Printing the points
-    print "" 
+    print ""
     print "The input points are:"
     print str(points)
     print ""
-    
+
     points, adjListMap = growSimpleRRT(points)
 
-    # Search for a solution  
-    path = basicSearch(adjListMap, 1, 20)    
+    # Search for a solution
+    path = basicSearch(adjListMap, 1, 20)
 
-    # Your visualization code 
-    displayRRTandPath(points, adjListMap, path) 
+    # Your visualization code
+    displayRRTandPath(points, adjListMap, path)
 
     # Solve a real RRT problem
     RRT(robot, obstacles, (x1, y1), (x2, y2))
-    
-    # Your visualization code 
+
+    # Your visualization code
     displayRRTandPath(points, adjListMap, path, robotStart, robotGoal, obstacles)
 
 
